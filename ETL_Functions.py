9ab544dd3056c097cbc3bfac94895740d91c5978
SP17-RedChromePlugin <willyscrugs@gmail.com>
2025-10-01 10:54:11 -0400

    Bug fixes and more comments for clarity

--- ETL_Functions.py
+++ ETL_Functions.py
@@ -48,6 +48,7 @@ def NHTSA_APICall(stateCode, countyCode, startYear, endYear):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
 
+    #Does not accept requests without proper header
     response = requests.get(url, headers=headers, timeout=30)
     data = response.json()
     
@@ -68,12 +69,13 @@ def NHTSA_GetCaseSpecifics(db, crash_df, stateCode):
 
     conn = sqlite3.connect(db)
 
+    #To avoid unnecessary API calls, the following code calls in the case_specifics table if it exists and then skips cases that are already logged.
     caseSpecDB = pd.DataFrame(columns=["state_case","year","month","day"])
     tempdb = pd.read_sql_query(f"SELECT name FROM sqlite_master WHERE type='table' AND name='case_specifics'", conn)
-    if len(tempdb["name"]) > 0: #case specifics table already exists in database
+    if len(tempdb["name"]) > 0:
         caseSpecDB = pd.read_sql_query("SELECT * FROM case_specifics", conn)
 
-    for index, row in crash_df.iterrows():
+    for _, row in crash_df.iterrows():
         scase = row["state_case"]
         year = row["year"]
 
@@ -82,7 +84,9 @@ def NHTSA_GetCaseSpecifics(db, crash_df, stateCode):
         else:
             url = f"https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseDetails?stateCase={scase}&caseYear={year}&state={stateCode}&format=json"
 
-            new_df = NHTSA_CaseSpec_APICall(url, scase)
+            #This code was originally less compact and was performing the below code in two separate places, which is why this is a separate function.
+            #Even though it isn't necessary anymore, it seems more readable to keep the API call a separate function.
+            new_df = NHTSA_CaseSpec_APICall(url, scase, sleep_time=3)
             if new_df is not None:
                 caseSpecDB = pd.concat([caseSpecDB, new_df])
 
@@ -90,12 +94,12 @@ def NHTSA_GetCaseSpecifics(db, crash_df, stateCode):
     
 
 
-def NHTSA_CaseSpec_APICall(url, scase):
+def NHTSA_CaseSpec_APICall(url, scase, sleep_time):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
 
-    time.sleep(3) #Being cautious since this will be sending multiple API calls in rapid succession when new crash specifics emerge
+    time.sleep(sleep_time) #Being cautious since this will be sending multiple API calls in rapid succession when new crash specifics emerge
 
     response = requests.get(url, headers=headers, timeout=30)
     data = response.json()
@@ -103,7 +107,8 @@ def NHTSA_CaseSpec_APICall(url, scase):
     new_df = pd.DataFrame([[scase, None, None, None]])
     if data["Count"] == 1: #Some cases don't have detailed information released yet, in which case the API will return a count of 0
 
-        #Since other date values in the datasets I'm working with have leading 0s in month and day values, I am adding them in here
+        #Since other date values in the datasets I'm working with have leading 0s in month and day values, I am adding them in here.
+        #These leading zeroes are why I treat month and day values as TEXT in the schema.
         year = data["Results"][0][0]['CrashResultSet']['YEAR']
         month = f"{int(data["Results"][0][0]['CrashResultSet']['MONTH']):02}"
         day = f"{int(data["Results"][0][0]['CrashResultSet']['DAY']):02}"
@@ -118,6 +123,8 @@ def NHTSA_CaseSpec_APICall(url, scase):
 
 def OpenAQ_Sensor_APICall(sensorID, limit, page):
     
+    #I was originally avoiding services that required API keys, since keeping the key private is important,
+    #but due to needing to choose a third dataset before too much time had passed, I chose one that required it.
     client = OpenAQ(api_key="7755a70a98b5f75b8d6c20c291ea8d3bec9a8b18542a10126358c7d19f49a75a")
     response = client.measurements.list(
         sensors_id=sensorID,
@@ -168,6 +175,8 @@ def transform_accidents(data):
     return accident_df
 
 def transform_ozone_measure(data):
+
+    #Since I am using OpenAQ's API package, they return their data in custom data types, not typical python data types.
     ozone_df = pd.DataFrame(columns=["datetime","mean_values", "minimum_value", "maximum_value", "name", "units"])
     for item in data.results:
         date = item.period.datetime_from.local[:10]
@@ -189,6 +198,11 @@ Loading Function
 """
 
 def load_to_database(weather_df, crash_df, casespecs_df, ozone_df, db="crashinfo.db"):
+    """
+    With more time, I would have liked to have this load function append non-duplicate values to the tables
+    created in the schema. For now, this function replaces all values in the tables with new ones, 
+    so this pipeline will lose old data when the window of time passes it.
+    """
     conn = sqlite3.connect(db)
 
     weather_df.to_sql("weather", conn, if_exists="replace", index=False)
